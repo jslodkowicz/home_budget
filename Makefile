f8cf3a1d8427a4a6c98d3ec8152fa3913f0c63da
Jan Słodkowicz <jan.slod@gmail.com>
2020-01-22 21:46:57 +0100

    added Category and Transaction models, registered models in admin panel. Created makefile with deps command

---
+++ Makefile
@@ -0,0 +1,2 @@
+make deps:
+	sudo apt-get install python3.7-dev libmysqlclient-dev && pip install -r requirements.txt
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Transaction(models.Model):
+
+    TRANSACTION_TYPE = (
+        ('exp', 'expense'),
+        ('inc', 'income'),
+    )
+
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    category = models.ForeignKey(Category, on_delete=models.SET(None))
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'{self.amount} zl - {self.title}'
