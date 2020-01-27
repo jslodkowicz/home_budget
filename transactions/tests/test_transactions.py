fc29459e0bd6c93532c58256ff24a6ffb4c3aea1
Jan Słodkowicz <jan.slod@gmail.com>
2020-01-27 09:52:26 +0100

    modified transactions save method and updated tests

--- transactions/tests/test_transactions.py
+++ transactions/tests/test_transactions.py
@@ -28,3 +28,16 @@ class TransactionApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 2)
         self.assertEqual(res.data, serializer.data)
+
+    def test_amount_negative_when_expense(self):
+        wal = Wallet.objects.create()
+        cat = Category.objects.create(name='jedzenie')
+        exp = Transaction.objects.create(wallet=wal, title='ogórki', amount=10, type='exp', category=cat)
+        inc = Transaction.objects.create(wallet=wal, title='pizza', amount=40, type='inc', category=cat)
+
+        res = self.client.get(TRANSACTIONS_URL)
+
+        self.assertEqual(exp.amount, -10)
+        self.assertEqual(inc.amount, 40)
+        self.assertEqual(str(exp), '-10 zł - ogórki')
+        self.assertEqual(str(inc), '40 zł - pizza')
