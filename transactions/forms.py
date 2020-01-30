2d7ac595f6871f8c1b43145c0cf93e8e0bec37ee
jslodkowicz <jan.slod@gmail.com>
2020-01-30 19:30:47 +0100

    updated transaction detail

--- transactions/forms.py
+++ transactions/forms.py
@@ -6,11 +6,13 @@ from .models import Wallet
 class TransferForm(forms.Form):
     wallet_from = forms.ModelChoiceField(
         queryset=Wallet.objects.all(),
-        to_field_name='name'
+        to_field_name='name',
+        label='From'
     )
     wallet_to = forms.ModelChoiceField(
         queryset=Wallet.objects.all(),
-        to_field_name='name'
+        to_field_name='name',
+        label='To'
     )
     title = forms.CharField(max_length=100)
     amount = forms.DecimalField(max_digits=10, decimal_places=2)
