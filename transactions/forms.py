8a033d26687a08440c6e8e157eeff6ca1d257671
Jan Słodkowicz <jan.slod@gmail.com>
2020-02-03 10:51:33 +0100

    added validation for transaction on the same wallet

--- transactions/forms.py
+++ transactions/forms.py
@@ -14,3 +14,15 @@ class TransferForm(forms.Form):
     )
     title = forms.CharField(max_length=100)
     amount = forms.DecimalField(max_digits=10, decimal_places=2)
+
+    def clean(self):
+
+        super().clean()
+
+        wallet1 = self.cleaned_data['wallet_from']
+        wallet2 = self.cleaned_data['wallet_to']
+
+        if wallet1 == wallet2:
+            self.errors['wallet_to'] = self.error_class([
+                'Wallet From cannot be the same as Wallet To'
+            ])
