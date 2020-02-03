f26e7c1fe6346961cbfa20e62db4bfbf8fa01210
Jan Słodkowicz <jan.slod@gmail.com>
2020-02-03 12:24:11 +0100

    updated forms.py

--- transactions/forms.py
+++ transactions/forms.py
@@ -16,13 +16,9 @@ class TransferForm(forms.Form):
     amount = forms.DecimalField(max_digits=10, decimal_places=2)
 
     def clean(self):
-
         super().clean()
 
-        wallet1 = self.cleaned_data['wallet_from']
-        wallet2 = self.cleaned_data['wallet_to']
-
-        if wallet1 == wallet2:
+        if self.cleaned_data['wallet_from'] == self.cleaned_data['wallet_to']:
             self.errors['wallet_to'] = self.error_class([
                 'Wallet From cannot be the same as Wallet To'
             ])
