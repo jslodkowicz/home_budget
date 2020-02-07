2c2917ef88e20d4162296c1c9b83d12265127ae0
Jan Słodkowicz <jan.slod@gmail.com>
2020-02-07 16:34:02 +0100

    simple invitation method works properly

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('<pk>/profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
+
+from .models import Profile
 
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'accounts/profile.html'
