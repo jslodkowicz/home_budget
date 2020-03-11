5508b1e3257eccfeaed4d472ec129eac68d3a45f
Jan Słodkowicz <jan.slod@gmail.com>
2020-03-11 16:21:22 +0100

    flake8

--- transactions/utils.py
+++ transactions/utils.py
@@ -7,4 +7,5 @@ API_URL = 'https://free.currconv.com/api/v7/convert?q={}_{}' \
 
 def currency_converter(cur_1: str, cur_2: str) -> dict:
     """Function used to calculate current exchange rates for currency pairs"""
-    return requests.get(API_URL.format(cur_1, cur_2, os.getenv("CURRENCY_KEY"))).json()
+    return requests.get(API_URL.format(cur_1, cur_2, os.getenv(
+        "CURRENCY_KEY"))).json()
