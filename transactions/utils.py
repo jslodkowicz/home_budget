9df2cabcaea8277ee5ebd1bb1778472049b99e6f
Jan Słodkowicz <jan.slod@gmail.com>
2020-03-11 16:17:56 +0100

    added .env file

--- transactions/utils.py
+++ transactions/utils.py
@@ -1,9 +1,10 @@
+import os
 import requests
 
 API_URL = 'https://free.currconv.com/api/v7/convert?q={}_{}' \
-          '&compact=ultra&apiKey=938b70c522258c63f39d'
+          '&compact=ultra&apiKey={}'
 
 
 def currency_converter(cur_1: str, cur_2: str) -> dict:
     """Function used to calculate current exchange rates for currency pairs"""
-    return requests.get(API_URL.format(cur_1, cur_2)).json()
+    return requests.get(API_URL.format(cur_1, cur_2, os.getenv("CURRENCY_KEY"))).json()
