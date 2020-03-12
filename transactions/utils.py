0abf60d1d12cbd6a9441c3c4d4502d7509f87ade
Jan Słodkowicz <jan.slod@gmail.com>
2020-03-12 16:16:29 +0100

    updated currency converter variable names

--- transactions/utils.py
+++ transactions/utils.py
@@ -5,7 +5,7 @@ API_URL = 'https://free.currconv.com/api/v7/convert?q={}_{}' \
           '&compact=ultra&apiKey={}'
 
 
-def currency_converter(cur_1: str, cur_2: str) -> dict:
+def currency_converter(currency_from: str, currency_to: str) -> dict:
     """Function used to calculate current exchange rates for currency pairs"""
-    return requests.get(API_URL.format(cur_1, cur_2, os.getenv(
+    return requests.get(API_URL.format(currency_from, currency_to, os.getenv(
         "CURRENCY_KEY"))).json()
